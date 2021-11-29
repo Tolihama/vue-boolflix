c822aa29e88caddf6fa75421a90b0690690d78ad
Tolihama <fra070@gmail.com>
2021-11-29 10:47:45 +0100

    Project setup

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <input type="text">
+        <button>Search</button>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
